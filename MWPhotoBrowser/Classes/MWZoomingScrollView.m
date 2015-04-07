776d5910044b89a707f29cb0e15e9ae8271883e0
Kyle Lanchman <k.lanchman@gmail.com>
2015-04-06 21:12:49 -0400

    Change maxScale calculation

--- MWPhotoBrowser/Classes/MWZoomingScrollView.m
+++ MWPhotoBrowser/Classes/MWZoomingScrollView.m
@@ -249,11 +249,7 @@
     CGFloat minScale = MIN(xScale, yScale);                 // use minimum of these to allow the image to become fully visible
 
     // Calculate Max
-	CGFloat maxScale = 3;
-    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
-        // Let them go a bit bigger on a bigger screen!
-        maxScale = 4;
-    }
+    CGFloat maxScale = ceil(yScale);
     
     // Image is smaller than screen so no zooming!
 	if (xScale >= 1 && yScale >= 1) {
